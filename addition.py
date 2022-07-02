93817e5b6dec36e6aa8f256b45237f8f4cf104e6
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2022-07-02 21:12:27 +0530

    python

--- addition.py
+++ addition.py
@@ -2,5 +2,9 @@ num1 = int(input("Enter the value of first no : ")
 num2 = int(input("Enter the value of second no : ")
 
 sum = num1 + num2:
+sub = num1 - num2:
+mul = num1 * num2:
 
 print("The additon of two no is :-",sum)
+print("The additon of two no is :-",sub)
+print("The additon of two no is :-",mul)
--- initializing_char _into _interger.py
+++ initializing_char _into _interger.py
@@ -2,4 +2,6 @@ a = 10
 b = 20
 # Output
 print("the valve of a is",a)
+
+
  
