7722f3273a48a2519e606f6fa2543925096d2675
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2022-08-17 22:08:15 +0530

    variable

---
+++ Variables.py
@@ -0,0 +1,59 @@
+#Variabless
+x = 4 # X is a variable assing value integer as 
+y = "Shreyash" # Y is string assing vaule name
+print(x) # Print the out put
+print(y)
+
+# Casting variables
+a = str(5) # its a string
+b = int(3) # its a integer
+c = float(4) # its a float which as decimal value 4.o
+print(c)
+print(type(y)) # to get type of variable
+
+#Case sensitive
+H = 4
+h = "How"
+print(H)
+print(h)
+#A will not overwrite a
+
+# Variable nameing in python
+sovar = "hi"
+so_var = "hello"
+_var_v = "ki"
+SOVAR = "hey"
+sovar3 = "enjoy"
+# Camel case
+soVariableName = "sheryash"
+# Pascal case
+SoVariableName = 4
+# Space case (snake case)\
+so_variable_name = "shreyash"
+
+# Mutiple value in varialbes
+p, m, l = "Orange", "Banana", "Cherry"
+print(p)
+print(m)
+print(l)
+
+# collection of variables
+cars = ["Mercedes", "BMW", "Audi"]
+v, f, r = cars
+print(v)
+print(f)
+print(r)
+
+# Output variables
+t = "Python "
+u = "is "
+k = "awesome"
+print(t + u + k)
+
+# Global variable
+q = "awesome"
+
+def myfunc():
+  print("Python is " + q)
+
+myfunc()
---
+++ helloworld.py
@@ -0,0 +1,5 @@
+print("Hello, world and my learning of python starts form here...")
+# This is how you comment in python
+
+""" for long comments
+#  do this """
