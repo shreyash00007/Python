df7321d64a3bb10e1619c730d2e51ff1d546198f
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2022-08-17 21:33:47 +0530

    python

---
+++ Solved problems/cars.py
@@ -0,0 +1,6 @@
+cars  = ['audi','bmw','toyota','tata']
+for car in cars:
+    if car  == 'bmw':
+       print(car.upper())
+    else:
+        print(car.title())
--- initializing_char _into _interger.py
+++
@@ -1,7 +0,0 @@
-a = 10
-b = 20
-# Output
-print("the valve of a is",a)
-
-
- 
