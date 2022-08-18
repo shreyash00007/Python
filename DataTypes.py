7d692c9b8695f8c2674baea250f1b1e5c0df8307
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2022-08-18 22:50:16 +0530

    Data Types

---
+++ DataTypes.py
@@ -0,0 +1,27 @@
+from platform import python_branch
+from typing import Sequence
+
+
+x = 5
+print(type(x)) # To get Data Type
+
+"""
+Types of Data Type in python
+
+Numeric - int, float, complex
+
+Sequence - list, tuple, range
+
+Text-type - str #string
+
+Mapping type - dict
+
+Set Type - set, frozenst
+
+Boolean Type - bool (true and false)
+
+Binary type - bytes, byterarry, memoryview
+
+None type - NoneType
+
+"""
