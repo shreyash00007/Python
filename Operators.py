c49ee7272e070e20df5d042231ea959b35b57222
shreyash dhanawade <shreyashdhanawadecode@gmail.com>
2022-08-19 12:04:39 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,54 @@
+print(2 + 6) # Operators in python
+
+"""
+Types of operators in python
+
+Arithmetic operators
++ Addition
+- Subtraction
+* Mutiplication
+/ Division
+% Modulus
+** Exponentiation
+// Floor division
+
+Assignment Operators
+
+=  equal to 
++=  add equal to 
+-=  sub equal to 
+*=  mul equal to 
+/=  div equal to
+
+Comparison Operators
+
+== equal 
+!= Not equal
+>  Greter than 
+<  less than
+>=  Greater than or equal to 
+<= Less than or equal to
+
+Logical Operators
+
+and  1 if both are 11
+or   1 if one is 1 
+not  revers 1 to 0
+
+Membership Operators
+
+in  	Returns True if a sequence with the
+        specified value is present in the object
+not in  Returns True if a sequence with the
+        specified value is not present in the object 
+
+Bitwise Operators
+
+&  AND
+|  OR
+^ XOR
+~ NOT
+<< zero fill left shift
+>> Signed right shift
+
+"""
